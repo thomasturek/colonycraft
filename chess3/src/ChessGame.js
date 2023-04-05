24c200b4eaa352b512d5fb0af25ef565147dfa76
thomasturek <thomasturekpersonal@gmail.com>
2023-04-05 02:06:36 +0200

    update

--- chess3/src/ChessGame.js
+++ chess3/src/ChessGame.js
@@ -1,96 +1,109 @@
-import React, {useEffect, useState} from "react";
-import Chessboard from 'chessboardjsx';
-import socketIO from 'socket.io-client';
-import { Chess } from 'chess.js';
-import './ChessGame.css';
+import React, { useEffect, useState } from "react";
+import Chessboard from "chessboardjsx";
+import socketIO from "socket.io-client";
+import { Chess } from "chess.js";
+import "./ChessGame.css";
 
-const socket = socketIO.connect('http://localhost:8080');
+const socket = socketIO.connect("http://54.163.168.91:3000", {
+  transports: ["websocket"],
+  cors: {
+    origin: "*",
+  },
+});
 
 const ChessGame = () => {
-
   const [fen, setFen] = useState(new Chess().fen());
   const [highlight, setHighlight] = useState({});
-  const [room, setRoom] = useState(""); // current room
-  const [player, setPlayer] = useState(""); // current player (white or black)
-  const [chatInput, setChatInput] = useState(""); // chat input text
-  const [chatHistory, setChatHistory] = useState([]); // chat history
+  const [room, setRoom] = useState("");
+  const [player, setPlayer] = useState("");
+  const [chatInput, setChatInput] = useState("");
+  const [chatHistory, setChatHistory] = useState([]);
+  const [isMyTurn, setIsMyTurn] = useState(false);
 
   useEffect(() => {
+
     socket.on("connect", () => {
-      setRoom("exampleRoom"); // replace with your desired room name
-      socket.emit("joinRoom", "exampleRoom"); // replace with your desired room name
+
+      if (room !== null) {
+        socket.emit("joinRoom", room);
+      } else {
+        const roomName = prompt("Please enter a room name:");
+        setRoom(roomName);
+        socket.emit("joinRoom", room);
+      }
     });
 
-    socket.on("move", (data) => {
-      handleMove(data);
+    socket.on("move", function (move) {
+      handleMove(move);
     });
 
-    socket.on("chat", (data) => {
-      setChatHistory([...chatHistory, data]);
+    socket.on("player", function (player) {
+      setPlayer(player);
+    });
+
+    socket.on("turn", function () {
+      setIsMyTurn(true);
+    });
+
+    socket.on("gameover", function (result) {
+      alert(`Game over. ${result}`);
     });
   }, []);
 
   const handleSelect = (square) => {
-  const chess = new Chess(fen);
 
-  const moves = chess.moves({ square: square });
+    const chess = new Chess(fen);
 
-  const newHighlight = { [square]: 'yellow' };
+    const moves = chess.moves({ square: square });
 
-  moves.forEach((move) => {
-    newHighlight[move] = 'green';
-  });
+    const newHighlight = { [square]: "red" };
 
-  setHighlight({...highlight, ...newHighlight});
-};
+    moves.forEach((move) => {
+      newHighlight[move] = "green";
+    });
 
+    setHighlight({ ...highlight, ...newHighlight });
+  };
 
   const handleMove = (move) => {
-  const chess = new Chess(fen);
-
-  try {
-    const result = chess.move(move);
-
-    if (result === null) {
-      throw new Error("Invalid move");
-    }
+    const chess = new Chess(fen);
 
     setFen(chess.fen());
     setHighlight({});
-  } catch (error) {
-    console.log(error.message);
-    // Revert the piece to its original square
+
+    setIsMyTurn(true);
+  };
+
+  const movePiece = (move) => {
+    const chess = new Chess(fen);
+
     setFen(chess.fen());
     setHighlight({});
-  }
-  socket.emit("move", move);
-};
 
-const handleChatSubmit = (e) => {
-    e.preventDefault();
-    if (chatInput.trim()) {
-      const data = {
-        message: chatInput,
-        sender: player,
-        timestamp: Date.now(),
-      };
-      socket.emit("chat", data);
-      setChatInput("");
-    }
+    socket.emit("move", { room, move });
+
+    setIsMyTurn(false);
   };
 
   return (
-    <div className="chessboard-wrapper">
-      <Chessboard
-        position={fen}
-        onSquareClick={handleSelect}
-        onDrop={(move) => handleMove({ from: move.sourceSquare, to: move.targetSquare, promotion: "q" })}
-        squareStyles={highlight}
-        dropSquareStyle={{ boxShadow: "inset 0 0 1px 4px #90EE90" }}
-        className="chessboard"
-      />
-    </div>
-  );
+      <div className="chessboard-wrapper">
+        <div className="room-info">
+          Room: {room}
+          <br />
+          It is {isMyTurn ? `your turn (${player})` : `${player === 'white' ? 'black' : 'white'}'s turn`}
+        </div>
+        <Chessboard
+          position={fen}
+          orientation={player}
+          onSquareClick={handleSelect}
+          onDrop={(move) => movePiece({ from: move.sourceSquare, to: move.targetSquare, promotion: "q" })}
+          squareStyles={highlight}
+          dropSquareStyle={{ boxShadow: "inset 0 0 1px 4px #90EE90" }}
+          className="chessboard"
+        />
+
+      </div>
+    );
 };
 
 export default ChessGame;
